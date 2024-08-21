4c4fbed03e8ff00e3663db4bb57cad45d99f3504
AXEL - CNCNOC <AXEL - CNCNOC@NOC>
2024-08-21 10:54:00 -0500

    CLASES UTP

---
+++ Semana2/UTP/src/Semana2/Ejercicio1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana2;
+
+/**
+ *
+ * @author AXEL - CNCNOC
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        double n1=1;
+        double n2=2;
+        System.out.println("El resultado es: "+(n1+n2));
+    }
+    
+}
